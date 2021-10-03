f8299d7c61d84aefa1a3f7afd937e7d7aeb2074b
RJRicher <rricher@live.com>
2021-10-02 22:02:03 -0400

    initial upload

---
+++ HTML/BasicHTMLandJavaScript1/BasicJavaScript1.js
@@ -0,0 +1,1 @@
+alert("This is my JavaScript alert box");
---
+++ HTML/BasicHTMLandJavaScript1/BasicJavaScript1b.js
@@ -0,0 +1,6 @@
+function MyFirstFunction() {
+			var String = "Kiss me, I'm Irish!";
+			var result = String.fontcolor("green");
+			document.getElementById("irish").innerHTML =
+			result;
+		}
